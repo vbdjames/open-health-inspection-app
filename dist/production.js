fceca8c614296f1e941af36237f9b791b0f61aa0
Blaine Price <william.price@homes.com>
2015-01-30 15:51:34 -0500

    Clear out for a geolocation button.

--- dist/production.js
+++ dist/production.js
@@ -75,17 +75,15 @@ var openHealthDataAppControllers =
   angular.module('openHealthDataAppControllers', []);
 
 openHealthDataAppControllers.controller('modalController',
-  ['$scope', '$modalInstance', 'items', '$log', '$location', 
-  function($scope, $modalInstance, items, $log, $location){
-
-  $scope.ok = function () {
-    // $log.info('http://code4hr.eventbrite.com/?aff=busapp');
-    // $location.replace('http://eventbrite.com');
-    $location.href="http://code4hr.eventbrite.com/?aff=busapp";
-  };
+  ['$scope', '$modalInstance', 'items', '$log', '$location', '$window',
+  function($scope, $modalInstance, items, $log, $location, $window){
 
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
+  };
+
+  $scope.zipcode = function () {
+    debugger;
   };
 
 }]);
